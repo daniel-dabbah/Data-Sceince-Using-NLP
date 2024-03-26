d3ac44ec81183d849f5069457812cf81e3d848f6
daniel-dabbah <daniel.dabbah12@gmail.com>
2024-03-26 21:03:25 +0200

    finish preprocess on date cols

--- main.py
+++ main.py
@@ -7,33 +7,36 @@ Created on Mon Mar 18 18:26:12 2024
 """
 import torch
 import pandas as pd
+import pre_process
 
 df = pd.read_csv("data/train.csv.gz")
 
-a = df[:10]
-cols_to_exclude = ['Unnamed: 0', 'OBJECTID', 'FOD_ID', 'FPA_ID',
-                   'LOCAL_FIRE_REPORT_ID', 'LOCAL_INCIDENT_ID']
 
-cols_to_use = ['SOURCE_SYSTEM_TYPE',]
+cols_to_exclude = ['Unnamed: 0', 'OBJECTID', 'FOD_ID', 'FPA_ID',
+                   'LOCAL_FIRE_REPORT_ID', 'LOCAL_INCIDENT_ID',
+                   'ICS_209_INCIDENT_NUMBER', 'ICS_209_NAME', 'MTBS_ID',
+                   'MTBS_FIRE_NAME', 'COMPLEX_NAME', 'OWNER_CODE', 'Shape']
+
+cols_to_use = ['SOURCE_SYSTEM_TYPE', 'FIRE_YEAR', 'DISCOVERY_DATE',
+               'DISCOVERY_DOY', 'DISCOVERY_TIME', 'STAT_CAUSE_DESCR',
+               'CONT_DATE', 'CONT_DOY', 'CONT_TIME', 'FIRE_SIZE',
+               'FIRE_SIZE_CLASS', 'LATITUDE', 'LONGITUDE', 'STATE', ]
 
 
 cols_to_check_value_counts = ['SOURCE_SYSTEM', 'NWCG_REPORTING_AGENCY',
                               'NWCG_REPORTING_UNIT_ID',
                               'NWCG_REPORTING_UNIT_NAME', 'SOURCE_REPORTING_UNIT',
-                              'SOURCE_REPORTING_UNIT_NAME',]
+                              'SOURCE_REPORTING_UNIT_NAME', 'FIRE_CODE',
+                              'FIRE_NAME', 'OWNER_DESCR', 'COUNTY', 'FIPS_CODE', 'FIPS_NAME'
+                              ]
 
-a.columns
+if __name__ == '__main__':
 
-# , 'FIRE_CODE', 'FIRE_NAME',
-#     'ICS_209_INCIDENT_NUMBER', 'ICS_209_NAME', 'MTBS_ID', 'MTBS_FIRE_NAME',
-#     'COMPLEX_NAME', 'FIRE_YEAR', 'DISCOVERY_DATE', 'DISCOVERY_DOY',
-#     'DISCOVERY_TIME', 'STAT_CAUSE_DESCR', 'CONT_DATE', 'CONT_DOY',
-#     'CONT_TIME', 'FIRE_SIZE', 'FIRE_SIZE_CLASS', 'LATITUDE', 'LONGITUDE',
-#     'OWNER_CODE', 'OWNER_DESCR', 'STATE', 'COUNTY', 'FIPS_CODE',
-#     'FIPS_NAME', 'Shape'],
-
-
-# COLUMNS_TO_USE = ['FIRE_SIZE', 'FIRE_SIZE_CLASS', 'FIRE_YEAR', 'DISCOVERY_DATE',
-#                   'DISCOVERY_DOY', 'DISCOVERY_TIME', 'CONT_DATE', 'CONT_DOY',
-#                   'CONT_TIME', 'STAT_CAUSE_DESCR',  # The most important Feature
-# ]
+    df = pd.read_csv("data/train.csv.gz", usecols=cols_to_use)
+    df = pre_process.pre_process_time_cols(df)
+    a = df[:10]
+    # check Avi affected features
+    # remove unwanted features
+    # make month and day to a string.
+    # maybe use fire size
+# len(cols_to_check_value_counts) + len(cols_to_exclude) + len(cols_to_use)
---
+++ pre_process.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 18:35:07 2024
+
+@author: danieldabbah
+"""
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import datetime
+
+
+def fill_and_convert_julian_to_gregorian(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    """
+    Preprocesses a pandas DataFrame by calculating the average Julian date for non-null values in a specified column,
+    filling null values with this average, and converting Julian dates to Gregorian dates.
+
+    Parameters:
+    - df: pandas DataFrame containing the data.
+    - column_name: the name of the column containing Julian dates to process.
+
+    Returns:|
+    - A pandas DataFrame with the specified column's null values filled with the average Julian date and converted to Gregorian dates.
+    """
+
+    # Step 1: Calculate the average Julian date for non-null values
+    average_julian_date = df[column_name].dropna().mean()
+
+    # Step 2: Fill nulls in the specified column with the calculated average
+    df[column_name].fillna(average_julian_date, inplace=True)
+
+    # Step 3: Apply the Julian to Gregorian conversion logic to the column
+    df[column_name] = df[column_name].apply(lambda julian_date: datetime.datetime.fromtimestamp(
+        (julian_date - 2440587.5) * 86400.0).strftime('%Y-%m-%d'))
+
+    return df
+
+
+def impute_and_format_doy(df, column_name='CONT_DOY'):
+    """
+    Imputes missing values in the day-of-year column and formats the column by converting to integers.
+
+    Parameters:
+    - df: pandas DataFrame containing the data.
+    - column_name: String name of the column with day-of-year values.
+
+    Returns:
+    - A modified pandas DataFrame with missing day-of-year values imputed and all values formatted as integers.
+    """
+
+    # Calculate the median day-of-year, ignoring NaN values
+    median_doy = df[column_name].median()
+
+    # Fill NaN values with the median day-of-year
+    df[column_name].fillna(median_doy, inplace=True)
+
+    # Convert day-of-year values to integers to remove any decimals
+    df[column_name] = df[column_name].astype(int)
+
+    return df
+
+
+def categorize_and_impute_time(df, time_column):
+    # Convert all non-NaN values to strings for consistent processing
+    df[time_column] = df[time_column].apply(
+        lambda x: f"{int(x):04}" if not pd.isnull(x) else np.nan)
+
+    # Calculate the median time, ignoring NaN values
+    median_time = df[time_column].dropna().astype(int).median()
+
+    # Convert the median time to a string in HHMM format
+    median_time_str = f"{int(median_time):04}"
+
+    # Fill NaN values with the median time
+    df[time_column].fillna(median_time_str, inplace=True)
+
+    # Convert time to HH:MM format for readability
+    df[time_column] = df[time_column].apply(lambda x: f"{x[:2]}:{x[2:]}")
+    # Function to categorize time into part of the day
+
+    def categorize_time(time_str):
+        hour = int(time_str[:2])
+        if 5 <= hour <= 11:
+            return 'Morning'
+        elif 12 <= hour <= 16:
+            return 'Afternoon'
+        elif 17 <= hour <= 20:
+            return 'Evening'
+        else:  # Covers night and early morning hours
+            return 'Night'
+
+    # Apply categorization to each time
+    df['CATEGORY_'+time_column] = df[time_column].apply(categorize_time)
+    return df
+
+
+def get_season_optimized(date):
+    month = date.month
+    day = date.day
+    if month in (1, 2, 12):
+        season = 'Winter'
+    elif month in (3, 4, 5):
+        season = 'Spring'
+    elif month in (6, 7, 8):
+        season = 'Summer'
+    else:
+        season = 'Fall'  # Renaming Autumn to Fall
+
+    # Adjusting for days in transitional months
+    if month == 3 and day >= 21:
+        season = 'Spring'
+    elif month == 6 and day >= 21:
+        season = 'Summer'
+    elif month == 9 and day >= 23:
+        season = 'Fall'
+    elif month == 12 and day >= 21:
+        season = 'Winter'
+
+    return season
+
+
+def preprocess_time_cols(df):
+
+    # convering from julian to gregorian
+    # this if for time when the fire was discover
+    df = fill_and_convert_julian_to_gregorian(df, 'DISCOVERY_DATE')
+    df = impute_and_format_doy(df, 'DISCOVERY_DOY')
+    df['disc_time'] = df['DISCOVERY_TIME']
+    df = categorize_and_impute_time(df, 'DISCOVERY_TIME')
+
+    # Fill NaN values with the median:
+    df['disc_time'].fillna(df['disc_time'].median(), inplace=True)
+
+    # this is when the fire was under control
+    df = fill_and_convert_julian_to_gregorian(df, 'CONT_DATE')
+    df = impute_and_format_doy(df, 'CONT_DOY')
+    df = categorize_and_impute_time(df, 'CONT_TIME')
+
+    # Applying the optimized function to the dataframe
+    df['DISCOVERY_DATE'] = pd.to_datetime(df['DISCOVERY_DATE'])
+
+    # finding the season of the year
+    df['SEASON'] = df['DISCOVERY_DATE'].apply(get_season_optimized)
+
+    # the time difrence when the fire was discover to when it is under control
+    df['DAYS_DIFFERENCE'] = df.apply(lambda row: row['CONT_DOY'] - row['DISCOVERY_DOY'] if row['CONT_DOY']
+                                     >= row['DISCOVERY_DOY'] else (365 - row['DISCOVERY_DOY']) + row['CONT_DOY'], axis=1)
+    # Step 1: Convert DISCOVERY_TIME and CONT_TIME to timedelta objects
+    df['DISCOVERY_TIME_DELTA'] = pd.to_timedelta(
+        df['DISCOVERY_TIME'].str.strip() + ':00')
+    df['CONT_TIME_DELTA'] = pd.to_timedelta(
+        df['CONT_TIME'].str.strip() + ':00')
+
+    # Step 2: Calculate the initial time difference in minutes
+    df['TIME_DIFF_MINUTES'] = (
+        df['CONT_TIME_DELTA'] - df['DISCOVERY_TIME_DELTA']).dt.total_seconds() / 60
+
+    # Step 3: Adjust for the difference in days, assuming 1440 minutes in a day (24 hours * 60 minutes)
+    df['ADJUSTED_TIME_DIFF_MINUTES'] = df.apply(lambda row: row['TIME_DIFF_MINUTES'] if row['CONT_DOY'] >= row['DISCOVERY_DOY']
+                                                else row['TIME_DIFF_MINUTES'] + (1440 * (row['CONT_DOY'] - row['DISCOVERY_DOY'] + 365)), axis=1)
+    # Step 2: Calculate the initial time difference in minutes
+    df['TIME_DIFF_MINUTES'] = df['TIME_DIFF_MINUTES'] + \
+        df['DAYS_DIFFERENCE']*24*60
+    df = df.drop(['CONT_TIME_DELTA', 'DISCOVERY_TIME_DELTA',
+                 'ADJUSTED_TIME_DIFF_MINUTES', 'CATEGORY_CONT_TIME', 'DAYS_DIFFERENCE'], axis=1)
+
+    df = df.drop(columns=['CONT_DATE', 'CONT_DOY', 'CONT_TIME'])
+
+    df['d_of_month'] = df['DISCOVERY_DATE'].dt.day
+    df['month'] = df['DISCOVERY_DATE'].dt.month
+    df = df.drop(['DISCOVERY_DATE'], axis=1)
+    df = df.drop(['DISCOVERY_TIME'], axis=1)
+
+    return df
